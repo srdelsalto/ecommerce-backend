aec44746b41d86cbb36a4c21bc76f92578d59207
Santiago Del Salto <santyrdse@gmail.com>
2025-04-06 00:30:31 -0500

    Feature (Ecommerce): Added dependencies

---
+++ ecommerce/src/test/java/ec/com/trycatch/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.trycatch.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
